0eb86e89e781b85168100eed2bf0478af7830d3c
RamiroCovian <ramiro.covian@gmail.com>
2023-08-29 11:23:03 -0300

    refactor: optimizo codigo en barajar()

--- funcionamiento.py
+++ funcionamiento.py
@@ -2,14 +2,28 @@ from main import Baraja
 
 # Ejemplo de uso
 
-prueba = Baraja()  # Armo el naipe
-print(prueba.naipes)
 
-print(prueba.crearBaraja())  # Creo la Baraja
+juego = Baraja()  # Armo el naipe
+print(Baraja())
+print("PASO 1------------------------------------")
+
+print(juego.naipes)
+print("PASO 2------------------------------------")
+
+print(juego.crearBaraja())  # Creo la Baraja
+print("PASO 3------------------------------------")
+
 print(
-    prueba.repartir()
+    juego.repartir()
 )  # Reparto las ultimas cartas de la lista, serian las de arriba o primeras del mazo.
-print(prueba.naipes)  # Imprimo naipe para ver cartas restantes
+print("PASO 4------------------------------------")
 
-prueba.barajar()  # Barajo las veces por defecto (3) el naipe
-print(prueba.naipes)  # Imprimo naipe para corroborar que se haya barajado correctamente
+print(juego.naipes)  # Imprimo naipe para ver cartas restantes
+print("PASO 5------------------------------------")
+
+juego.barajar()  # Barajo las veces por defecto (3) el naipe
+print("PASO 6------------------------------------")
+
+print(juego.naipes)  # Imprimo naipe para corroborar que se haya barajado correctamente
+print("PASO 7------------------------------------")
+print(juego.repartir())
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 class Baraja:
     def __init__(self):
         # Oros, Copas, Espadas, Bastos
-        NUMEROS = ["A", "2", "3", "4", "5", "6", "7", "S", "C", "R"]
+        numeros = ["A", "2", "3", "4", "5", "6", "7", "S", "C", "R"]
         # Numeros por palo
-        PALOS = ["o", "c", "e", "b"]
+        palos = ["o", "c", "e", "b"]
         # # Naipe, mezclar ambos palos con numeros
-        self.naipes = [(numero + palo) for numero in NUMEROS for palo in PALOS]
-        # self.baraja_inicial = self.crearBaraja()
+        self.naipes = [(numero + palo) for numero in numeros for palo in palos]
 
     def crearBaraja(self):
         # Convierto en set ya que son desordenados
@@ -16,44 +15,40 @@ class Baraja:
 
         return self.naipes
 
-    def mezclar(self):
-        n = len(self.naipes)
-        mitad = n // 2  # Divido a la mitad la cantidad de cartas en el baraja
-
-        primera_mitad = self.naipes[:mitad]  # ":" Operador de corte
-        segunda_mitad = self.naipes[mitad:]
-
-        self.naipes.clear()  # Limpio la lista para dejarla en 0
-
-        for i in range(mitad):
-            self.naipes.append(
-                segunda_mitad[i]
-            )  # Agrego de forma intercalada los elementos de la primer mitad con los de la segunda mitad
-            self.naipes.append(primera_mitad[i])
-
-        if n % 2 != 0:
-            self.naipes.append(
-                segunda_mitad[-1]
-            )  # Si la baraja tiene un número impar de cartas, la última carta de la segunda mitad se agrega al final
-
-        return self.naipes
-
     def barajar(
         self, veces=3
     ):  # Establezco un numero predeterminado de veces que quiero que se mezcle la baraja
-        for _ in range(veces):
-            self.mezclar()
+        for v in range(veces):
+            n = len(self.naipes)
+            mitad = n // 2  # Divido a la mitad la cantidad de cartas en el baraja
+
+            # ":" Operador de corte , aca seria desde el inicio hasta :
+            primera_mitad = self.naipes[:mitad]
+            segunda_mitad = self.naipes[mitad:]
+
+            self.naipes.clear()  # Limpio la lista para dejarla en 0
+
+            for i in range(mitad):
+                self.naipes.append(
+                    segunda_mitad[i]
+                )  # Agrego de forma intercalada los elementos de la primer mitad con los de la segunda mitad
+                self.naipes.append(primera_mitad[i])
+
+            return self.naipes
 
     def repartir(
         self, jugadores=2, mano=3
     ):  # Establezco como predeterminado que participan 2 jugadores y se reparten 3 cartas para cada uno.
         # Mano: cantidad de cartas que se reparten por jugador
         manos = []
-        for _ in range(jugadores):
+        for j in range(jugadores):
             mano_jugador = []
-            for _ in range(mano):
+            for m in range(mano):
                 mano_jugador.append(
                     self.naipes.pop()
                 )  # .pop metodo para entregar los elementos de la posicion que uno desee, en este caso de los ultimos y lo elimina de self.naipes
             manos.append(mano_jugador)
         return manos
+
+    def __str__(self):
+        return f"Es hora de jugar!!!"
